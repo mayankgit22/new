f5c2dc56012d43f0bd74e133c675037e9eb241ef
mayank kumar <162879224+mayankgit22@users.noreply.github.com>
2024-06-04 16:30:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let Name ;
+document.getElementById("mysubmit").onclick=function(){
+     Name=document.getElementById('myinput').value;
+    // console.log(Name)
+    document.getElementById("myh1").textContent=`Hello ${Name}  
+    Congratulation!! You are not nigga`
+}
